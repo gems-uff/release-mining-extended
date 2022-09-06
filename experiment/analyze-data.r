d5329619ccbac4a05dcc1d8cb91258c8150fb5e1
Felipe Curty <felipecrp@gmail.com>
2022-09-06 09:49:38 -0300

    Add RQ2.c

--- experiment/analyze-data.r
+++ experiment/analyze-data.r
@@ -18,7 +18,7 @@ releases_bproj <- releases %>% group_by(project) %>%
     time_naive_fmeasure = mean(time_naive_fmeasure),
     time_expert_precision = mean(time_expert_precision),
     time_expert_recall = mean(time_expert_recall),
-    time_expert_fmeasure = mean(time_expert_fmeasure),
+    time_expert_fmeasure = mean(time_expert_fmeasure)
   )
 
 releases_bproj_melted <- releases_bproj %>% 
@@ -161,6 +161,30 @@ releases_committers_bproj_melted <- releases_committers_bproj %>%
     range_fmeasure.many = mean(range_fmeasure.many)
   ) %>% round(4)
 
+wilcox.test(
+  releases_committers_bproj$time_precision.few,
+  releases_committers_bproj$time_precision.many,
+  paired = TRUE)
+cliff.delta(
+  releases_committers_bproj$time_precision.few,
+  releases_committers_bproj$time_precision.many)
+
+wilcox.test(
+  releases_committers_bproj$time_recall.few,
+  releases_committers_bproj$time_recall.many,
+  paired = TRUE)
+cliff.delta(
+  releases_committers_bproj$time_recall.few,
+  releases_committers_bproj$time_recall.many)
+
+wilcox.test(
+  releases_committers_bproj$range_precision.few,
+  releases_committers_bproj$range_precision.many,
+  paired = TRUE)
+cliff.delta(
+  releases_committers_bproj$range_precision.few,
+  releases_committers_bproj$range_precision.many)
+
 releases_committers_bproj_melted %>%
   filter(
     variable == "time_precision.few" | variable == "time_precision.many" |
@@ -292,6 +316,149 @@ releases_bases_bproj_melted %>%
     xlab("") +
     theme_bw(base_size = 12)
 ggsave("../paper/figs/rq_factors_base_bp_recall.png", width = 8, height = 2)
+
+#%
+# RQ2.c
+
+releases_wcy <- releases %>% na.omit()
+cycle_treshold <- mean(releases_wcy$cycle_sec)
+
+releases_rapid_bproj <- releases_wcy %>% 
+  filter(cycle_sec < cycle_treshold) %>%
+  group_by(project) %>% 
+  summarise(time_precision = mean(time_precision),
+            time_recall = mean(time_recall),
+            time_fmeasure = mean(time_fmeasure),
+            range_precision = mean(range_precision),
+            range_recall = mean(range_recall),
+            range_fmeasure = mean(range_fmeasure),
+            releases = n())
+
+releases_trad_bproj <- releases_wcy %>% 
+  filter(cycle_sec > cycle_treshold) %>%
+  group_by(project) %>% 
+  summarise(time_precision = mean(time_precision),
+            time_recall = mean(time_recall),
+            time_fmeasure = mean(time_fmeasure),
+            range_precision = mean(range_precision),
+            range_recall = mean(range_recall),
+            range_fmeasure = mean(range_fmeasure),
+            releases = n())
+
+releases_speed_bproj <- releases_rapid_bproj %>% 
+  inner_join(releases_trad_bproj, by = "project",
+      suffix = c(".few", ".many"))
+
+releases_speed_bproj_melted <- releases_speed_bproj %>% 
+  select(-releases.few, -releases.many) %>%
+  gather(variable, value, -project) %>%
+  mutate(
+    group = case_when(grepl("few", variable) ~ "few", TRUE ~ "many"),
+    strategy = case_when(grepl("time", variable) ~ "time",
+                               grepl("range", variable) ~ "range",
+                               TRUE ~ "fmeasure"),
+    variable = factor(variable, levels = c(
+      "time_precision.many",
+      "time_recall.many",
+      "time_fmeasure.many",
+      "range_precision.many",
+      "range_recall.many",
+      "range_fmeasure.many",
+      "time_precision.few",
+      "time_recall.few",
+      "time_fmeasure.few",
+      "range_precision.few",
+      "range_recall.few",
+      "range_fmeasure.few"
+    ))
+  )
+
+100 * releases_speed_bproj %>%
+  summarise(
+    time_precision.few = mean(time_precision.few),
+    time_precision.many = mean(time_precision.many),
+    range_precision.few = mean(range_precision.few),
+    range_precision.many = mean(range_precision.many),
+    time_recall.few = mean(time_recall.few),
+    time_recall.many = mean(time_recall.many),
+    range_recall.few = mean(range_recall.few),
+    range_recall.many = mean(range_recall.many),
+    time_fmeasure.few = mean(time_fmeasure.few),
+    time_fmeasure.many = mean(time_fmeasure.many),
+    range_fmeasure.few = mean(range_fmeasure.few),
+    range_fmeasure.many = mean(range_fmeasure.many),
+  ) %>% round(4)
+
+wilcox.test(
+  releases_speed_bproj$time_precision.few,
+  releases_speed_bproj$time_precision.many,
+  paired = TRUE)
+cliff.delta(
+  releases_speed_bproj$time_precision.few,
+  releases_speed_bproj$time_precision.many)
+
+wilcox.test(
+  releases_speed_bproj$time_recall.few,
+  releases_speed_bproj$time_recall.many,
+  paired = TRUE)
+cliff.delta(
+  releases_speed_bproj$time_recall.few,
+  releases_speed_bproj$time_recall.many)
+
+wilcox.test(
+  releases_speed_bproj$range_precision.few,
+  releases_speed_bproj$range_precision.many,
+  paired = TRUE)
+cliff.delta(
+  releases_speed_bproj$range_precision.few,
+  releases_speed_bproj$range_precision.many)
+
+releases_speed_bproj_melted %>%
+  filter(
+    variable == "time_precision.few" | variable == "time_precision.many" |
+    variable == "range_precision.few" | variable == "range_precision.many"
+  ) %>%
+  ggplot(aes(x=variable, y=value)) +
+    ggtitle("Precision - rapid vs traditional releases") +
+    geom_boxplot() +
+    coord_flip() +
+    scale_x_discrete(labels=c(
+      "time_precision.many" = "many",
+      "time_precision.few" =  "few",
+      "range_precision.many" = "many",
+      "range_precision.few" =  "few"
+    ), position = "top") +
+    facet_grid(rows = vars(strategy), scales = "free",
+        switch = "y", labeller = as_labeller(c(
+      "time" = "time-based",
+      "range" = "range-based"
+    ))) +
+    ylab("") + scale_y_continuous(labels = scales::percent, limits = c(0.75, 1)) +
+    xlab("") +
+    theme_bw(base_size = 12)
+#ggsave("../paper/figs/rq_factors_bp_collaborators_precision.png", width = 8, height = 3)
+
+releases_speed_bproj_melted %>%
+  filter(variable == "time_recall.few" | variable == "time_recall.many") %>%
+  ggplot(aes(x=variable, y=value)) +
+    ggtitle("Recall - rapid vs traditional releases") +
+    geom_boxplot() +
+    coord_flip() +
+    scale_x_discrete(labels=c(
+      "time_recall.many" = "many",
+      "time_recall.few" =  "few"
+    ), position = "top") +
+    facet_grid(rows = vars(strategy), scales = "free",
+        switch = "y", labeller = as_labeller(c(
+      "time" = "time-based",
+      "range" = "range-based"
+    ))) +
+    ylab("") + scale_y_continuous(labels = scales::percent, limits = c(0.4, 1)) +
+    xlab("") +
+    theme_bw(base_size = 12)
+# ggsave("../paper/figs/rq_factors_bp_collaborators_recall.png", width = 8, height = 2)
+
+
 
 # RQ3. Time vs Naive
 wilcox.test(releases_bproj$time_precision, releases_bproj$time_naive_precision, paired = TRUE)
